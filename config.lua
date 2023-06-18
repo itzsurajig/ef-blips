11686ad06448cc73676e3416b253a5e763dd3fe9
BlasterSuraj <104319683+blastersuraj@users.noreply.github.com>
2023-06-18 13:24:49 +0530

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,39 @@
+Config = Config or {}
+
+---EF-Blips  
+Config.blipsShow = true
+
+Config.Locations = {
+[1] = {
+    vector = vector3(538.32, -183.18, 54.47), 
+    text = "Repair Station", 
+    color = 1, 
+    sprite = 446, 
+    scale = 0.5,
+},
+[2] = {
+    vector = vector3(-778.01, 4965.29, 209.68), 
+    text = "Hunting Zone", 
+    color = 5, 
+    sprite = 432, 
+    scale = 0.5,
+},
+[3] = {
+    vector = vector3(-72.05, 6252.36, 31.09),
+    text = "Chicken Factory", 
+    color = 26, 
+    sprite = 273, 
+    scale = 0.5,
+},
+-- Example 
+-- [[
+-- [4] = {
+--     vector = vector3(0, 0, 0),
+--     text = "Blip Name", 
+--     color = 3, 
+--     sprite = Blip ID, 
+--     scale = size,
+-- }, ]]
+
+--- add more
+}
