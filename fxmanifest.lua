fdd21a9541a461a4c42446dbc2abfb0e8ec1e3f8
BlasterSuraj <104319683+blastersuraj@users.noreply.github.com>
2023-06-18 13:28:39 +0530

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+
+
+author 'blastersuraj'
+description 'Blip Creator Free For QBCore and ESX'
+version '1.0'
+scriptname 'ef-blips'
+
+
+
+shared_script 'config.lua'
+client_script 'efblips.lua'
